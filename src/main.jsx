bc5aa8efa26e34ddf3a2165854b12054335d8801
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-05-19 16:07:49 -0700

    chore: install Material UI
    Installed packages related to Material UI.
    
    Set up main.jsx to use Material UI.
    
    Cleared index.css, because the CSS properties are unlikely to be used.
    Standard base CSS properties have been put in instead.
    
    Set up theme structure, but currently is empty, because theme has not
    been finalized yet.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App.jsx';
+import theme from './theme.js';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 
 // ==================================================
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
