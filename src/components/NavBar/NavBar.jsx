428b75f0c1d56f6eacf1096fd02829939d41aa10
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-05-23 15:03:24 -0700

    feat: create logo
    Created a logo with the gear icon from svgrepo.com.
    
    Used logo as the website's favicon.
    
    Used logo in the NavBar component.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,33 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import { useContext } from 'react';
 
 import NavLink from './components/NavLink';
+import logo from '/src/assets/logo.svg';
+import { AppContext } from '/src/contexts';
 
 // ==================================================
 
 const sections = ['About', 'Skills', 'Projects'];
 
 function NavBar() {
+  const { closeProject } = useContext(AppContext);
+
+  // --------------------------------------------------
+
+  const styles = { '& img.logo': { cursor: 'pointer' } };
+
   return (
-    <AppBar className="NavBar" position="sticky" component="nav">
+    <AppBar className="NavBar" position="sticky" component="nav" sx={styles}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        logo
+        <img
+          src={logo}
+          className="logo"
+          alt="logo"
+          width="32"
+          onClick={closeProject}
+        />
         <Box>
           {sections.map((section) => (
             <NavLink key={section} href={`#${section.toLowerCase()}`}>
