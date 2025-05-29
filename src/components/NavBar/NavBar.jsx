d3464a4bd9a7156d56124bc14095fb3761c1363f
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-05-28 20:42:19 -0700

    feat: click logo to redirect to home
    The click handling, for clicking on the logo in the NavBar, was a hack
    to go back to the home page.  This hack was done by changing a state in
    the App component.  A better way of going back to the home page and
    starting over would be to make the logo an anchor link.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,23 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import { useContext } from 'react';
 
 import NavLink from './components/NavLink';
 import logo from '/src/assets/logo.svg';
-import { AppContext } from '/src/contexts';
 
 // ==================================================
 
 const sections = ['About', 'Skills', 'Projects'];
 
 function NavBar() {
-  const { closeProject } = useContext(AppContext);
-
-  // --------------------------------------------------
-
   const styles = { '& img.logo': { cursor: 'pointer' } };
 
   return (
     <AppBar className="NavBar" position="sticky" component="nav" sx={styles}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <img
-          src={logo}
-          className="logo"
-          alt="logo"
-          width="32"
-          onClick={closeProject}
-        />
+        <a href="/">
+          <img src={logo} className="logo" alt="logo" width="32" />
+        </a>
         <Box>
           {sections.map((section) => (
             <NavLink key={section} href={`#${section.toLowerCase()}`}>
