8a5f167c4563976c95de9b0bc1e477c298b50e43
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-06-11 14:36:40 -0700

    feat: put project GitHub links into icons
    On the frontend project page, the GitHub link to a project was plainly
    written out as text.  The link is now made into a GitHub icon, so that
    it is easier to identify.

--- src/components/ProjectPage/components/ProjectFirstSection.jsx
+++ src/components/ProjectPage/components/ProjectFirstSection.jsx
@@ -1,10 +1,22 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import parse from 'html-react-parser';
 
 // ==================================================
 
 function ProjectFirstSection({ project }) {
+  const links = [...project.links];
+
+  let githubLink;
+  const githubIndex = links.findIndex((link) =>
+    link.startsWith('https://github.com')
+  );
+  if (githubIndex > -1) {
+    githubLink = links.splice(githubIndex, 1)[0];
+  }
+
   return (
     <Box className="ProjectPage__ProjectFirstSection" component="section">
       <Container>
@@ -12,11 +24,24 @@ function ProjectFirstSection({ project }) {
           className="ProjectPage__ProjectFirstSection__deployed-links"
           sx={{ margin: '1em 0' }}
         >
-          {project.links.map((link) => (
+          {links.map((link) => (
             <p key={link}>
               <a href={link}>{link}</a>
             </p>
           ))}
+        </Box>
+        <Box className="ProjectPage__ProjectFirstSection__icon-links">
+          {githubLink && (
+            <IconButton
+              href={githubLink}
+              target="_blank"
+              rel="noreferrer"
+              size="large"
+              aria-label="redirect"
+            >
+              <GitHubIcon fontSize="inherit" />
+            </IconButton>
+          )}
         </Box>
         {parse(project.sections.first)}
       </Container>
